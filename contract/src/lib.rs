bea859ed1d831d5cd3f62e9657bca582c47d6caa
Libo Shen <liboooshen@gmail.com>
2022-09-12 11:19:13 +0100

    Linkdrop init implementation

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -15,6 +15,7 @@ NOTES:
   - To prevent the deployed contract from being modified or deleted, it should not have any access
     keys on its account.
 */
+use near_contract_standards::non_fungible_token::approval;
 use near_contract_standards::non_fungible_token::core::NonFungibleTokenCore;
 use near_contract_standards::non_fungible_token::core::NonFungibleTokenResolver;
 use near_contract_standards::non_fungible_token::events::NftMint;
@@ -32,11 +33,13 @@ use near_sdk::{
 
 mod facai_gen;
 mod karma;
+mod linkdrop;
 
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct OldContract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
+    karma: karma::Karma,
 }
 
 #[near_bindgen]
@@ -45,6 +48,7 @@ pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
     karma: karma::Karma,
+    linkdrop: linkdrop::Linkdrops,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml;charset=UTF-8,%3csvg viewBox='0 0 1077 1080' xmlns='http://www.w3.org/2000/svg' xml:space='preserve' fill-rule='evenodd' clip-rule='evenodd' stroke-linecap='round' stroke-linejoin='round' stroke-miterlimit='1.6'%3e%3cpath fill='none' d='M0 0h1076v1079H0z'/%3e%3cpath d='M650 6169v19H544c13-41 75-74 159-81l-1 123-231-1c19-93 134-165 273-165 140 0 256 74 273 169l-231-2s-4-117-3-123c81 8 144 41 155 82l-98-1v-24' fill='none' stroke='gray' stroke-width='27.3' transform='matrix(1.00905 0 0 1.04494 -206 -6074)'/%3e%3cpath d='M650 6169v19H544c13-41 75-74 159-81l-1 123-231-1c19-93 134-165 273-165 140 0 256 74 273 169l-231-2-3-123c81 8 144 41 155 82l-98-1v-24' fill='none' stroke='gray' stroke-width='27.3' transform='matrix(-1.009 .01008 -.01043 -1.04488 1359 6998)'/%3e%3cpath d='M504 363h80a14 14 0 0 0 0-29h-80a14 14 0 0 0 0 29ZM585 569l-79-1a14 14 0 0 0 0 29h79a14 14 0 0 0 0-28ZM735 491v-47a14 14 0 0 0-29 0v47a14 14 0 0 0 29 0ZM385 484v-46a14 14 0 0 0-29-1v47a14 14 0 0 0 29 0Z' fill='gray'/%3e%3cpath d='M474 3843c-13 5-28 7-43 7-42 0-79-20-97-51v-205c18 30 54 51 97 51 15 0 29-3 42-7l1 205ZM858 3539c9-2 17-5 26-10 36-19 57-56 54-93 8-13 13-28 14-45v189a108 108 0 0 1-14 62c3 36-17 73-54 93-9 4-17 7-26 9v-205ZM858 3744c-7 25-25 48-51 61-19 10-39 14-58 12l-1-206a102 102 0 0 0 110-73v206Z' fill='none' stroke='gray' stroke-width='32.9' transform='translate(-11 -2900)'/%3e%3cpath d='M749 3817c-14 21-37 37-65 43-26 6-52 1-73-11v-205c21 12 47 16 73 11 28-6 50-23 64-44l1 206ZM611 3849c-17 14-40 23-65 23-29 0-54-11-72-29l-1-205c18 17 44 29 73 29 25 0 48-9 65-23v205Z' fill='none' stroke='gray' stroke-width='32.9' transform='translate(-11 -2900)'/%3e%3cpath d='M334 3799c-48-2-88-40-93-89v-205c5 48 44 87 93 89v205Z' fill='none' stroke='gray' stroke-width='32.9' transform='translate(-12 -2900)'/%3e%3cpath d='M241 3710a101 101 0 0 1-61-97c-7-14-11-29-12-45h0v-1a107 107 0 0 1 0-8v-193c1 15 5 29 12 41v4c0 42 25 78 61 94v205Z' fill='none' stroke='gray' stroke-width='32.9' transform='translate(-12 -2901)'/%3e%3cpath d='M528 3074c18-15 43-24 69-23 31 1 58 15 76 37 19-4 40-2 60 7 23 10 41 28 52 49a99 99 0 0 1 89 79 98 98 0 0 1 59 106 105 105 0 0 1 0 104c2 36-18 73-55 93-8 4-17 7-26 9-7 25-25 47-51 61-19 10-39 14-58 12-14 21-37 37-65 43-26 6-52 1-73-11-17 14-40 23-65 23-28 0-54-11-72-29-13 5-28 7-43 7-42 0-79-20-97-51-48-2-87-40-93-89a101 101 0 0 1-61-98 103 103 0 0 1 17-121c2-48 36-87 81-96 8-24 27-47 53-61 21-12 43-17 64-15 13-21 35-38 61-45 28-7 56-3 78 9Z' fill='none' stroke='gray' stroke-width='32.9' transform='translate(-7 -2895)'/%3e%3c/svg%3e";
@@ -57,6 +61,7 @@ enum StorageKey {
     Approval,
     Karma,
     KarmaQuota,
+    LinkdropPending,
 }
 
 #[near_bindgen]
@@ -93,16 +98,18 @@ impl Contract {
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
             karma: karma::Karma::new(StorageKey::Karma, StorageKey::KarmaQuota),
+            linkdrop: linkdrop::Linkdrops::new(StorageKey::LinkdropPending),
         }
     }
 
     #[init(ignore_state)]
-    pub fn migrate() -> Self {
+    pub fn migrate_2022_09_07_linkdrop() -> Self {
         let old_state: OldContract = env::state_read().expect("failed");
         Self {
             tokens: old_state.tokens,
             metadata: old_state.metadata,
-            karma: karma::Karma::new(StorageKey::Karma, StorageKey::KarmaQuota),
+            karma: old_state.karma,
+            linkdrop: linkdrop::Linkdrops::new(StorageKey::LinkdropPending),
         }
     }
 
@@ -184,6 +191,43 @@ impl Contract {
 
     pub fn top_rank(&self) -> &Vec<(Balance, AccountId)> {
         self.karma.rank()
+    }
+
+    #[payable]
+    pub fn nft_linkdrop_init(&mut self, token_id: TokenId, pub_key: String) {
+        let owner_id = self.tokens.owner_by_id.get(&token_id).unwrap();
+        assert_eq!(env::predecessor_account_id(), owner_id, "Unauthorized");
+        self.tokens
+            .nft_approve(token_id.clone(), env::current_account_id(), None);
+        let approval_id: u64 = self
+            .tokens
+            .approvals_by_id
+            .as_ref()
+            .unwrap()
+            .get(&token_id)
+            .unwrap()
+            .get(&env::current_account_id())
+            .unwrap()
+            .clone();
+        self.linkdrop
+            .add_drop(&pub_key, &token_id, approval_id.clone());
+    }
+
+    #[payable]
+    pub fn nft_linkdrop_exec(
+        &mut self,
+        pub_key: String,
+        signature: String,
+        receiver_id: AccountId,
+    ) {
+        let linkdrop = self.linkdrop.get_drop(&pub_key, signature);
+        self.nft_transfer(
+            receiver_id,
+            linkdrop.token,
+            Some(linkdrop.approval_id),
+            None,
+        );
+        self.linkdrop.remove_drop(&pub_key);
     }
 }
 
@@ -276,7 +320,6 @@ impl NonFungibleTokenMetadataProvider for Contract {
 mod tests {
     use near_sdk::test_utils::{accounts, VMContextBuilder};
     use near_sdk::testing_env;
-    use std::collections::HashMap;
 
     use super::*;
 
